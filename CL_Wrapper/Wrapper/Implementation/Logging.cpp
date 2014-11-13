9fad45319bf58a55c606712dc28aebb49e32bac5
Manuel Maier <mjmaier@gmx.de>
2014-11-13 15:32:16 +0100

    Progress...

--- CL_Wrapper/Wrapper/Implementation/Logging.cpp
+++ CL_Wrapper/Wrapper/Implementation/Logging.cpp
@@ -2,9 +2,9 @@
 #include "Wrapper/Logging.h"
 #include "Wrapper/String.h"
 
-static mpLog::Block* g_pCurrentBlock;
+static mpInternal::LogBlock* g_pCurrentBlock;
 
-mpLog::Block::Block(const char* szFormat, ...) :
+mpInternal::LogBlock::LogBlock(const char* szFormat, ...) :
   m_pParent(g_pCurrentBlock),
   m_bHasLogged(false)
 {
@@ -12,63 +12,59 @@ mpLog::Block::Block(const char* szFormat, ...) :
   m_uiIndentation = m_pParent ? m_pParent->m_uiIndentation + 1 : 1;
 }
 
-mpLog::Block::~Block()
+mpInternal::LogBlock::~LogBlock()
 {
   g_pCurrentBlock = m_pParent;
   m_pParent = nullptr;
 }
 
-void mpLog::Block::LogDescription()
+void mpInternal::LogBlock::LogDescription()
 {
   if(m_bHasLogged)
     return;
 
 }
 
-
 namespace
 {
-  enum class LogLevel
-  {
-    Info,
-    Success,
-    Warning,
-    Error
-  };
 
-  const char* GetString(LogLevel Level)
+  const char* GetString(mpInternal::LogLevel Level)
   {
     switch(Level)
     {
-    case LogLevel::Info:    return "Info";
-    case LogLevel::Success: return "Success";
-    case LogLevel::Warning: return "Warning";
-    case LogLevel::Error:   return "Error";
+    case mpInternal::LogLevel::BlockBegin: return "BlockBegin";
+    case mpInternal::LogLevel::BlockEnd:   return "BlockEnd";
+    case mpInternal::LogLevel::Info:       return "Info";
+    case mpInternal::LogLevel::Success:    return "Success";
+    case mpInternal::LogLevel::Warning:    return "Warning";
+    case mpInternal::LogLevel::Error:      return "Error";
     }
     MP_NotImplemented;
     return nullptr;
   }
 
-  const char* GetShortString(LogLevel Level)
+  const char* GetShortString(mpInternal::LogLevel Level)
   {
     switch(Level)
     {
-    case LogLevel::Info:    return "Ifo";
-    case LogLevel::Success: return "Suc";
-    case LogLevel::Warning: return "Wrn";
-    case LogLevel::Error:   return "Err";
+    case mpInternal::LogLevel::BlockBegin: return ">>>";
+    case mpInternal::LogLevel::BlockEnd:   return "<<<";
+    case mpInternal::LogLevel::Info:       return "Ifo";
+    case mpInternal::LogLevel::Success:    return "Suc";
+    case mpInternal::LogLevel::Warning:    return "Wrn";
+    case mpInternal::LogLevel::Error:      return "Err";
     }
     MP_NotImplemented;
     return nullptr;
   }
 }
 
-static void LogToStdOut(LogLevel Level, const std::string& sMessage)
+static void LogToStdOut(mpInternal::LogLevel Level, const std::string& sMessage)
 {
   printf("%s| %s\n", GetShortString(Level), sMessage.c_str());
 }
 
-static void LogToVisualStudio(LogLevel Level, const std::string& sMessage)
+static void LogToVisualStudio(mpInternal::LogLevel Level, const std::string& sMessage)
 {
   OutputDebugStringA(GetShortString(Level));
   OutputDebugStringA("| ");
@@ -76,39 +72,54 @@ static void LogToVisualStudio(LogLevel Level, const std::string& sMessage)
   OutputDebugStringA("\n");
 }
 
-#define BODY(Level) \
-  std::stringstream outMessage; \
-  va_list vargs;\
-  va_start(vargs, szFormat);\
-  MP_OnScopeExit { va_end(vargs); };\
-  mpString::AppendFormatV(outMessage, szFormat, vargs);\
-  if(g_pCurrentBlock)\
-    for (mpUInt64 i = 0; i < g_pCurrentBlock->GetIndentation(); ++i)\
-    {\
-      outMessage << "  ";\
-    }\
-  auto sMessage = outMessage.str();\
-  LogToStdOut(Level, sMessage);\
-  LogToVisualStudio(Level, sMessage);
+#define FORMAT_MESSAGE(outMessage, szFormat) \
+  do{\
+    va_list vargs;\
+    va_start(vargs, szFormat);\
+    MP_OnScopeExit { va_end(vargs); };\
+    mpString::AppendFormatV(outMessage, szFormat, vargs);\
+  } while(false)
 
 void MP_WrapperAPI mpLog::Info(const char* szFormat, ...)
 {
-  BODY(LogLevel::Info);
+  std::stringstream ssMessage;
+  FORMAT_MESSAGE(ssMessage, szFormat);
+  DoLog(mpInternal::LogLevel::Info, ssMessage.str());
 }
 
 void MP_WrapperAPI mpLog::Success(const char* szFormat, ...)
 {
-  BODY(LogLevel::Success);
+  std::stringstream ssMessage;
+  FORMAT_MESSAGE(ssMessage, szFormat);
+  DoLog(mpInternal::LogLevel::Success, ssMessage);
 }
 
 void MP_WrapperAPI mpLog::Warning(const char* szFormat, ...)
 {
-  BODY(LogLevel::Warning);
+  std::stringstream ssMessage;
+  FORMAT_MESSAGE(ssMessage, szFormat);
+  DoLog(mpInternal::LogLevel::Warning, ssMessage);
 }
 
 void MP_WrapperAPI mpLog::Error(const char* szFormat, ...)
 {
-  BODY(LogLevel::Error);
+  std::stringstream ssMessage;
+  FORMAT_MESSAGE(ssMessage, szFormat);
+  DoLog(mpInternal::LogLevel::Error, ssMessage);
 }
 
-#undef BODY
+#undef FORMAT_MESSAGE
+
+void mpInternal::DoLog(LogLevel Level, std::stringstream& ssMessage)
+{
+  if(g_pCurrentBlock)
+  {
+    for(mpUInt64 i = 0; i < g_pCurrentBlock->GetIndentation(); ++i)
+    {
+      ssMessage << "  ";
+    }
+  }
+  auto sMessage = ssMessage.str();
+  LogToStdOut(Level, sMessage);
+  LogToVisualStudio(Level, sMessage);
+}
--- CL_Wrapper/Wrapper/Logging.h
+++ CL_Wrapper/Wrapper/Logging.h
@@ -1,26 +1,42 @@
 #pragma once
 
-namespace mpLog
+namespace mpInternal
 {
-  struct MP_WrapperAPI Block
+  enum class LogLevel
   {
-    Block(const char* szFormat, ...);
-    ~Block();
+    BlockBegin = -2,
+    BlockEnd = -1,
+
+    Info,
+    Success,
+    Warning,
+    Error
+  };
+
+  struct MP_WrapperAPI LogBlock
+  {
+    LogBlock(const char* szFormat, ...);
+    ~LogBlock();
 
     void LogDescription();
     mpUInt64 GetIndentation() const { return m_uiIndentation; }
 
   private:
-    Block* m_pParent;
+    LogBlock* m_pParent;
     std::string m_Description;
     mpUInt64 m_uiIndentation;
     bool m_bHasLogged;
   };
 
+  void MP_WrapperAPI DoLog(LogLevel Level, std::stringstream& ssMessage);
+}
+
+namespace mpLog
+{
   void MP_WrapperAPI Info(const char* szFormat, ...);
   void MP_WrapperAPI Success(const char* szFormat, ...);
   void MP_WrapperAPI Warning(const char* szFormat, ...);
   void MP_WrapperAPI Error(const char* szFormat, ...);
 }
 
-#define MP_LogBlock ::mpLog::Block MP_Concat(_LogBlock_, __LINE__)
+#define MP_LogBlock ::mpInternal::LogBlock MP_Concat(_LogBlock_, __LINE__)
