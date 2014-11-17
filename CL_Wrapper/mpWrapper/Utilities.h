557b39c8fc7d283ac70aa53d1a1136117f27c441
Manuel Maier <mjmaier@gmx.de>
2014-11-17 11:14:51 +0100

    Implemented mpRandom::mpNumberGenerator

--- CL_Wrapper/mpWrapper/Utilities.h
+++ CL_Wrapper/mpWrapper/Utilities.h
@@ -5,3 +5,4 @@
 #include "mpWrapper/Utilities/String.h"
 #include "mpWrapper/Utilities/Math.h"
 #include "mpWrapper/Utilities/Time.h"
+#include "mpWrapper/Utilities/Random.h"
---
+++ CL_Wrapper/mpWrapper/Utilities/Random.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <random>
+
+namespace mpRandom
+{
+  using RandomEngineType = std::default_random_engine;
+  using RandomDeviceType = std::random_device;
+
+  MP_WrapperAPI RandomDeviceType& GetRandomDevice();
+
+  class mpNumberGenerator
+  {
+  public:
+    mpNumberGenerator() { RandomizeSeed(); }
+
+    MP_ForceInline void RandomizeSeed() { SetSeed(GetRandomDevice()()); }
+    MP_ForceInline void SetSeed(mpUInt32 uiNewSeed) { m_uiSeed = uiNewSeed; m_Engine.seed(uiNewSeed); }
+    MP_ForceInline mpUInt32 GetSeed() const { return m_uiSeed; }
+    MP_ForceInline void Reset() { SetSeed(GetSeed()); }
+
+    template<typename IntegerType = cl_int>
+    IntegerType GenerateInteger(IntegerType iMin = 0, IntegerType iMax = std::numeric_limits<IntegerType>::max())
+    {
+        std::uniform_int_distribution<IntegerType> Generator(iMin, iMax);
+        return Generator(m_Engine);
+    }
+
+    template<typename FloatType = cl_float>
+    FloatType GenerateFloat(FloatType fMin = 0.0f, FloatType fMax = 1.0f)
+    {
+        std::uniform_real_distribution<FloatType> Generator(fMin, fMax);
+        return Generator(m_Engine);
+    }
+
+  private:
+    mpUInt32 m_uiSeed;
+    RandomEngineType m_Engine;
+  };
+}
