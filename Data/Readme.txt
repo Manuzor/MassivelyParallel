3397660be9f3340ac0a3fbc2b3e505e79d1b76b3
Manuel Maier <mjmaier@gmx.de>
2014-12-02 10:01:15 +0100

    Added balls. Rendering works as well

---
+++ Data/Readme.txt
@@ -0,0 +1,1 @@
+balls.png: Original from http://img3.wikia.nocookie.net/__cb20131217090807/degrassi/images/e/e4/Colorful-play-balls.jpg
--- Texture/Main.cpp
+++ Texture/Main.cpp
@@ -4,9 +4,36 @@
 #include "mpWrapper/Utilities.h"
 #include "mpWrapper/Foundation/Startup.h"
 
+class Object
+{
+public:
+  sf::String m_sTextureName;
+  sf::Texture m_Texture;
+  sf::Sprite m_Sprite;
+
+  void Initialize(const char* szTextureName)
+  {
+    m_sTextureName = szTextureName;
+    if(!m_Texture.loadFromFile(m_sTextureName))
+    {
+      mpLog::Error("Failed to load texture: %s", szTextureName);
+      MP_ReportError("Failed to load texture.");
+    }
+    m_Texture.setSmooth(true);
+
+    m_Sprite.setTexture(m_Texture);
+  }
+
+  void Draw(sf::RenderWindow& Window)
+  {
+    Window.draw(m_Sprite);
+  }
+};
+
 class Main : public mpApplication
 {
   sf::RenderWindow  m_Window;
+  Object m_Original;
   mpTime m_Beginning;
 
   virtual void PreStartup() final override
@@ -17,6 +44,20 @@ class Main : public mpApplication
   virtual void PostStartup() final override
   {
     m_Window.create(sf::VideoMode(800, 600), "Texture Tiling");
+    m_Original.Initialize("Data/balls.png");
+  }
+
+  void HandleEvent(sf::Event& event)
+  {
+    switch(event.type)
+    {
+    case sf::Event::KeyReleased:
+      if (event.key.code != sf::Keyboard::Escape)
+        break;
+    case sf::Event::Closed:
+      m_Window.close();
+      break;
+    }
   }
 
   virtual QuitOrContinue Run() final override
@@ -27,13 +68,13 @@ class Main : public mpApplication
     sf::Event event;
     while(m_Window.pollEvent(event))
     {
-      if (event.type == sf::Event::Closed)
-      {
-        m_Window.close();
-      }
+      HandleEvent(event);
     }
 
     m_Window.clear();
+
+    m_Original.Draw(m_Window);
+
     m_Window.display();
 
     return Continue;
@@ -43,8 +84,6 @@ class Main : public mpApplication
   {
     mpLog::Info("The application ran for %f seconds.",
                 (mpTime::Now() - m_Beginning).GetSeconds());
-    printf("\nPress any key to quit . . . ");
-    mpUtilities::GetSingleCharacter();
   }
 };
 
