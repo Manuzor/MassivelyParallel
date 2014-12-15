02e991a52da5101053819b5cdff3a33fd6817c93
Manuel Maier <mjmaier@gmx.de>
2014-12-15 21:07:43 +0100

    Debug breaks are now down 1 level higher on the stack

--- CL_Wrapper/mpWrapper/Common/Error.h
+++ CL_Wrapper/mpWrapper/Common/Error.h
@@ -2,17 +2,17 @@
 
 namespace mpInternal
 {
-  void MP_WrapperAPI ReportError(const char* szFile, size_t uiLine, const char* szMessage);
-  void MP_WrapperAPI AssertionFailed(const char* szFile, size_t uiLine, const char* szCondition, const char* szMessage);
-  void MP_WrapperAPI CLCallFailed(const char* szFile, size_t uiLine, const char* szCall, mpResult result);
+  bool MP_WrapperAPI ReportError(const char* szFile, size_t uiLine, const char* szMessage);
+  bool MP_WrapperAPI AssertionFailed(const char* szFile, size_t uiLine, const char* szCondition, const char* szMessage);
+  bool MP_WrapperAPI CLCallFailed(const char* szFile, size_t uiLine, const char* szCall, mpResult result);
 }
 
 MP_WrapperAPI const char* mpTranslateStatusCode(cl_int iStatusCode);
 
 #define MP_DebugBreak do { __debugbreak(); } while(false)
-#define MP_ReportError(...) mpInternal::ReportError(__FILE__, __LINE__, __VA_ARGS__)
-#define MP_Assert(condition, ...) do { if(!(condition)) { mpInternal::AssertionFailed(__FILE__, __LINE__, #condition, __VA_ARGS__); } } while(false)
+#define MP_ReportError(...) if (mpInternal::ReportError(__FILE__, __LINE__, __VA_ARGS__)) MP_DebugBreak;
+#define MP_Assert(condition, ...) do { if (!(condition)) { if (mpInternal::AssertionFailed(__FILE__, __LINE__, #condition, __VA_ARGS__)) MP_DebugBreak; } } while(false)
 
-#define MP_Verify(clCall) do { mpResult result = (clCall); if(result.Failed()) { mpInternal::CLCallFailed(__FILE__, __LINE__, #clCall, result); } } while(false)
+#define MP_Verify(clCall) do { mpResult result = (clCall); if(result.Failed()) { if (mpInternal::CLCallFailed(__FILE__, __LINE__, #clCall, result)) MP_DebugBreak; } } while(false)
 
 #define MP_NotImplemented MP_ReportError("Not implemented.")
--- CL_Wrapper/mpWrapper/Common/Implementation/Error.cpp
+++ CL_Wrapper/mpWrapper/Common/Implementation/Error.cpp
@@ -1,19 +1,19 @@
 #include "mpWrapper/PCH.h"
 
-void mpInternal::ReportError(const char* szFile, size_t uiLine, const char* szMessage)
+bool mpInternal::ReportError(const char* szFile, size_t uiLine, const char* szMessage)
 {
   mpLog::Error("=== Error Report ===\n%s(%u): %s", szFile, uiLine, szMessage);
-  MP_DebugBreak;
+  return true;
 }
 
-void mpInternal::AssertionFailed(const char* szFile, size_t uiLine, const char* szCondition, const char* szMessage)
+bool mpInternal::AssertionFailed(const char* szFile, size_t uiLine, const char* szCondition, const char* szMessage)
 {
   mpLog::Error("=== Assertion Failed ===\n%s(%u): %s\n  Message: %s", szFile, uiLine, szCondition, szMessage);
-  MP_DebugBreak;
+  return true;
 }
 
-void mpInternal::CLCallFailed(const char* szFile, size_t uiLine, const char* szCall, mpResult result)
+bool mpInternal::CLCallFailed(const char* szFile, size_t uiLine, const char* szCall, mpResult result)
 {
   mpLog::Error("CL call failed with status code \"%s\":\n%s(%u): %s", mpTranslateStatusCode(result.m_Status), szFile, uiLine, szCall);
-  MP_DebugBreak;
+  return true;
 }
