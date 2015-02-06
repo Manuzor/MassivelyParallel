6a290868d02efa13e55b0a9f638137e03d438d78
Manuel Maier <mjmaier@gmx.de>
2015-02-06 22:04:11 +0100

    Generating helper batch scripts now.

--- PackRelease.sh
+++ PackRelease.sh
@@ -15,6 +15,13 @@ files=(
   "Data/fern.png"
   "Data/Readme.txt"
 
+  # Kernels
+  # =======
+  "Kernels/Blend.cl"
+  "Kernels/PrefixSum.cl"
+  "Kernels/PrefixSum256.cl"
+  "Kernels/RadixSort.cl"
+
   # Built Binaries
   # ==============
   "Output/Bin/x64Release/mpWrapper.dll"
@@ -24,6 +31,21 @@ files=(
   "Output/Bin/x64Release/sfml-graphics-2.dll"
   "Output/Bin/x64Release/sfml-system-2.dll"
   "Output/Bin/x64Release/sfml-window-2.dll"
+
+  # Generated Files For a Release
+  # =============================
+  "RunExamTask1.bat"
+  "RunExamTask2.bat"
+  "RunExamTask3.bat"
 )
 
+# Generate the helper scripts.
+echo "start Output/Bin/x64Release/Texture.exe"   > RunExamTask1.bat
+echo "start Output/Bin/x64Release/PrefixSum.exe" > RunExamTask2.bat
+echo "start Output/Bin/x64Release/RadixSort.exe" > RunExamTask3.bat
+
+# Build the archive.
 7z a -mx9 -t7z "$packageName" ${files[@]}
+
+# Clean up generated files.
+rm RunExamTask1.bat RunExamTask2.bat RunExamTask3.bat
